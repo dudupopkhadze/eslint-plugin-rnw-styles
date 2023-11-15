ec6d7bf8cfece836b8d2352691de4a85f4b14e4b
Dudu Popkhadze <dudupopkhadze@gmail.com>
2023-11-15 12:46:31 +0400

    minor tweaks

--- rnw-styles.js
+++ rnw-styles.js
@@ -17,24 +17,32 @@ module.exports = {
       },
 
       "Program:exit"(node) {
+        const indexOfStyleSheet = node.tokens.findIndex(
+          (t) => t.type === "Identifier" && t.value === "createStyleSheet"
+        );
         [...styleKeys].forEach((key) => {
           const keyUsed = isKeyUsed(key, context);
           if (!keyUsed) {
-            const loc = findStyleDeclarationNode(node, key)?.loc;
+            const loc = findStyleDeclarationNode(
+              node,
+              key,
+              indexOfStyleSheet
+            )?.loc;
             if (loc)
               context.report({
                 loc,
-                message: `GOPEER GABAGOOL Style key '${key}' is defined but not used broo.`,
+                message: `Style key '${key}' is defined but not used.`,
               });
           }
         });
       },
     };
 
-    function findStyleDeclarationNode(node, styleName) {
-      return node.tokens.find(
-        (t) => t.type === "Identifier" && t.value === styleName
-      );
+    function findStyleDeclarationNode(node, styleName, indexOfStyleSheet) {
+      const finder = (t) => t.type === "Identifier" && t.value === styleName;
+      const token = node.tokens.slice(indexOfStyleSheet).find(finder);
+      if (token) return token;
+      return node.tokens.find(finder);
     }
 
     function isKeyUsed(key, context) {
